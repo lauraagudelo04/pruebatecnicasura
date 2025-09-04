e43a5bb4225caa6dd8a47a35535234ccec2cf297
Laura Agudelo <142184444+lauraagudelo04@users.noreply.github.com>
2025-09-04 13:52:23 -0500

    feat: agregar en util validacion para saber quien asume el gasto y calculo del iva a un monto

--- src/main/java/com/suraprueba/travelexpenses/repository/IEmployeeExpenseRepository.java
+++ src/main/java/com/suraprueba/travelexpenses/repository/IEmployeeExpenseRepository.java
@@ -1,11 +1,11 @@
 package com.suraprueba.travelexpenses.repository;
 
-import com.suraprueba.travelexpenses.domain.Employee;
+import com.suraprueba.travelexpenses.domain.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface IEmployeeExpenseRepository extends JpaRepository<Employee, UUID> {
+public interface IEmployeeExpenseRepository extends JpaRepository<Expense, UUID> {
 }
